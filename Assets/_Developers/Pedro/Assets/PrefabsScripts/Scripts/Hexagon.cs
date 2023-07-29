d212ccee8d79efafc91617482ee2982361bab1c3
Pepetrom <140914691+Pepetrom@users.noreply.github.com>
2023-07-29 16:49:52 -0300

    Update Hexagon.cs

--- Assets/_Developers/Pedro/Assets/PrefabsScripts/Scripts/Hexagon.cs
+++ Assets/_Developers/Pedro/Assets/PrefabsScripts/Scripts/Hexagon.cs
@@ -13,22 +13,22 @@ public class Hexagon : MonoBehaviour
     }
     private void Start()
     {
-        outline = GetComponent<Outline>();
-        outline.OutlineColor = Color.white;
+        //outline = GetComponent<Outline>();
+        //outline.OutlineColor = Color.white;
     }
     public void StartOutline()
     {
         if (!ocupado)
         {
             brilhando = true;
-            outline.OutlineColor = Color.red;
+          //  outline.OutlineColor = Color.red;
         }       
     }
 
     public void StopOutline()
     {
         brilhando = false;
-        outline.OutlineColor = Color.white;
+        //outline.OutlineColor = Color.white;
     }
 
     public bool IsOutlined()
