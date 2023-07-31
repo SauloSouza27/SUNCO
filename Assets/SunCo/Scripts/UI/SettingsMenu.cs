1f8fa5048fdc2b04a46fd7e5cfdb8d33899647c8
Vitor Soares Silva <vitrola.vitor@gmail.com>
2023-07-31 16:55:13 -0300

    Musica no menu

--- Assets/SunCo/Scripts/UI/SettingsMenu.cs
+++ Assets/SunCo/Scripts/UI/SettingsMenu.cs
@@ -1,12 +1,34 @@
-﻿using SunCo.Scripts.Managers;
+﻿using System;
+using SunCo.Scripts.Managers;
 using UnityEngine;
 using SunCo.Scripts.Utils;
+using UnityEngine.UI;
 
 namespace SunCo.Scripts.UI
 {
     public class SettingsMenu : MonoBehaviour
     {
+        [SerializeField] private Slider masterSlider;
+        [SerializeField] private Slider musicSlider;
+        [SerializeField] private Slider sfxSlider;
         
+        private void OnEnable()
+        {
+            SettingsManager.instance.Mixer.GetFloat("MasterVolume", out float masterVolume);
+            // Debug.Log(masterVolume + " " +  (masterVolume+80) / 80);
+            masterVolume = Mathf.Lerp(0, 1, (masterVolume + 80) / 80);
+            // Debug.Log(masterVolume);
+            masterSlider.value =  masterVolume;
+            
+            SettingsManager.instance.Mixer.GetFloat("MusicVolume", out float musicVolume);
+            musicVolume = Mathf.Lerp(0, 1, (musicVolume + 80) / 80);
+            musicSlider.value =  musicVolume;
+            
+            SettingsManager.instance.Mixer.GetFloat("SfxVolume", out float sfxVolume);
+            sfxVolume = Mathf.Lerp(0, 1, (sfxVolume + 80) / 80);
+            sfxSlider.value =  sfxVolume;
+        }
+
         public void UpdateMasterVolume(float newVolume)
         {
             SettingsManager.instance.Mixer.SetFloat("MasterVolume", Conversions.LogarithmicDbTransform(newVolume));
