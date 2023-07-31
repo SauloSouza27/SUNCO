8352567b51c45274bb7fe41ec3c30afe056ca972
saulosouza2703 <sauloas96@gmail.com>
2023-07-31 15:09:00 -0300

    Pasta Script Spawn Wave

---
+++ Assets/SunCo/Scripts/Wave e Spawn/Spawn.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    // Wich enemy
+    public GameObject inimigo;
+    // Wich position
+    public bool center, left, right;
+    private Vector3 posC, posL, posR;
+    private Vector3 maxX, m;
+    // Cooldowns
+}
