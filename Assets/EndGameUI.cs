68b109eddaa153b23a8e3f39a5607241d0d37637
Vitor Soares Silva <vitrola.vitor@gmail.com>
2023-07-31 23:22:50 -0300

    End game criado

---
+++ Assets/EndGameUI.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGameUI : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
