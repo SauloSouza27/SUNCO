5cc64b19745d22c805a1182bf324197271c57e8c
Pepetrom <140914691+Pepetrom@users.noreply.github.com>
2023-08-04 19:28:22 -0300

    Play Again funciona bem agora

--- Assets/SunCo/Scripts/UI/EndGameUI.cs
+++ Assets/SunCo/Scripts/UI/EndGameUI.cs
@@ -12,11 +12,13 @@ public class EndGameUI : MonoBehaviour
 
     public void PlayAgain()
     {
+        Time.timeScale = 1;
         SceneManager.LoadScene(1);
     }
 
     public void MainMenu()
     {
+        Time.timeScale = 1;
         SceneManager.LoadScene(0);
     }
 }
